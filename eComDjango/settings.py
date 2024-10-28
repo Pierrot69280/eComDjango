7fcb59f7a0e7518d2e35d656dcfcf88e4b6f71ee
Pierre <pierre.nogaro@gmail.com>
2024-10-28 08:42:09 +0100

    intégration bootstrap & model Product

--- eComDjango/settings.py
+++ eComDjango/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'bootstrap5',
+
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,12 @@ WSGI_APPLICATION = 'eComDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommerce',
+        'USER': 'ecom',
+        'PASSWORD': 'ecom',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- website/views.py
+++ website/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from website.models import Product
+
+
+def article_index(request):
+    articles = Product.objects.all()
+    return render(request, 'website/products/index.html', {'articles': articles})
