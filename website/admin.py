63e9afa0e8d8acb0b235185da1eddd7591bf17e9
Pierre <pierre.nogaro@gmail.com>
2024-10-30 17:55:46 +0100

    Remove create, edit, and delete permissions for Product and Category & add admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Category, Product, Order, OrderItem
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'category')
+    search_fields = ('title', 'description')
+    list_filter = ('category',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'is_completed')
+    search_fields = ('user__username',)
+    list_filter = ('is_completed',)
+    filter_horizontal = ('products',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+    search_fields = ('order__id', 'product__title')
